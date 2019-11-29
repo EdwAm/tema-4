63f91c2fa2ef431f92b6396cc8dd22d315c94858
EdwAm <54266518+EdwAm@users.noreply.github.com>
2019-11-29 11:40:20 +0100

    Oppgave 1-5

--- Oppgavesett-3/oppgave1/src/App.js
+++ Oppgavesett-3/oppgave1/src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
-import Oppmuntring from "./Oppmuntring";
+import Oppmuntring from "./components/Oppmuntring";
+import Bird from "./components/Bird";
+import Gud from "./components/Gud";
 
-function App() {
+const birds = ["seagull", "hummingbird", "blackbird", "parakeet"];
+
+const guder = [
+  {navn: "Balder", besk: "Ås, gud for lys, skjønnhet, kjærlighet og lykke"},
+  {navn: "Brage", besk: "Ås, gud for diktning, veltalenhet og skaldekunst"},
+  {navn: "Eir", besk: "Gudinne for legekunsten, en av Menglads Frøyas?) ni møyer"},
+  {navn: "Frøya", besk: "Vane, fruktbarhetsgudinne"}
+];
+
+
+function App () {
   return (
     <div className="App">    
       <Oppmuntring navn="Rick Astley" emne="Kunsten å synge rent" />
+      <ul>
+      { birds.map( (bird, index) => <Bird name={bird} key={index} /> ) }
+      </ul>
+      <h1>Norrøne guder</h1>
+      { guder.map( gud => <Gud navn={gud.navn} besk={gud.besk} /> ) }
     </div>
   );
-}
+}  
 
 export default App;
---
+++ Oppgavesett-3/oppgave1/src/components/Gud.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gud = (props) => {
+    return(
+        <div className="gud">
+            <h2>{props.navn}</h2>
+            <p>{props.besk}</p>
+        </div>
+    )
+}
+
+export default Gud
